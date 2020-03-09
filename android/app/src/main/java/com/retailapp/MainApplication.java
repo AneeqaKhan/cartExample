8474f507bd352baf04056471196fb2fa282db515
Nasir <naxir_sheikh@hotmail.com>
2020-03-09 20:16:44 +0500

    unlink slash screen package

--- android/app/src/main/java/com/retailapp/MainApplication.java
+++ android/app/src/main/java/com/retailapp/MainApplication.java
@@ -4,7 +4,6 @@ import android.app.Application;
 import android.content.Context;
 import com.facebook.react.PackageList;
 import com.facebook.react.ReactApplication;
-import org.devio.rn.splashscreen.SplashScreenReactPackage;
 import com.reactnativecommunity.asyncstorage.AsyncStoragePackage;
 import com.reactnativecommunity.netinfo.NetInfoPackage;
 import org.reactnative.maskedview.RNCMaskedViewPackage;
